46a861dc67f4caf94d4db829b1e6e2c07e1d10d7
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2022-12-01 22:12:39 -0300

    posts estilizados e criados pelo DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -61,5 +61,80 @@ function followSystem () {
     } 
 }
 
+function createUserById (id) {
+    let postUsers = document.createElement('div');
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === id) {
+            postUsers.classList.add('posts__users');
+
+            let img = document.createElement('img');
+
+            let postsUsersName = document.createElement('div');
+            postsUsersName.classList.add('posts__users--name');
+
+            let name = document.createElement('h2');
+
+            let stack = document.createElement('p');
+        
+            img.src = users[i].img;
+
+            name.innerText = users[i].user;
+                    
+            stack.innerText = users[i].stack;
+
+            postsUsersName.append (name, stack);
+                
+            postUsers.append(img, postsUsersName);
+        }
+    }
+    return postUsers;
+}
+
+function renderPosts (arr) {
+    let postsList = document.querySelector('#posts-list');
+
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement('li');   
+        li.id = arr[i].id_post;
+
+        let user = createUserById (arr[i].user);
+
+        let title = document.createElement('h2');
+        title.innerText = arr[i].title;
+
+        let text = document.createElement('p');
+        text.innerText = arr[i].text;
+
+        let postsButtons = document.createElement('div');
+        postsButtons.classList.add('posts__buttons');
+
+        let showPost = document.createElement('button');
+        showPost.dataset.showModal = arr[i].id_post;
+        showPost.innerText = 'Abrir Post';
+
+        let heartButtonDiv = document.createElement('div');
+        heartButtonDiv.classList.add('heart-button');
+
+        let heartButton = document.createElement('button');
+        heartButton.classList.add('heart-grey');
+
+        let buttonImg = document.createElement('img');
+        buttonImg.src = "./src/assets/img/heart_grey.svg";
+
+        heartButton.append(buttonImg);
+
+        let span = document.createElement('span');
+        span.innerText = 0;
+
+        heartButtonDiv.append(heartButton, span);
+
+        postsButtons.append(showPost, heartButtonDiv);
+
+        li.append(user, title, text, postsButtons);
+        postsList.appendChild(li);
+    }
+} 
+    
 renderUsersSugestions ()
 followSystem ()
+renderPosts (posts);
